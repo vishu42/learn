fb692807c034d1e0971ca0ae350271aa4e1a961d
Vishal Tewatia <tewatiavishal3@gmail.com>
2024-05-14 14:50:56 +0530

    update

--- dsa/leetcode/backtracking/letter-combinations-phone-number/main.go
+++ dsa/leetcode/backtracking/letter-combinations-phone-number/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"slices"
+	"sort"
 	"strings"
 )
 
@@ -38,50 +40,50 @@ func valid(solution string, r string) bool { // r = a, solution = ""
 	return c
 }
 
-// func charCombinations(str string, n int) []string { // str = abcdef, n = 2
-// 	// base condition
-// 	if len(sol) == n {
-// 		finalSol = append(finalSol, sol)
-// 		return finalSol
-// 	}
+func letterCombinationsBT(letters string, n int, startIndex int) {
 
-// 	x := len(digitToLetterMap[firstDigit])
-// 	for _, r := range str[0:x] {
-// 		if valid(sol, string(r)) { // sol = "", r = a
-// 			sol = sol + string(r)
-
-// 			charCombinations(str, n, firstDigit)
-
-// 			sol = sol[:len(sol)-1]
-// 		}
-// 	}
-// 	return finalSol
-// }
-
-func letterCombinationsBT(letterGroups []string, n int) {
 	if len(sol) == n {
 		finalSol = append(finalSol, sol)
 		return
 	}
 
-	for _, letterGroup = range letterGroups {
-		if valid()
+	for i := startIndex; i < len(letters); i++ {
+
+		sol = sol + string(letters[i])
+		letterCombinationsBT(letters, n, i+1)
+		sol = sol[:len(sol)-1]
+
 	}
 }
 
 func letterCombinations(digits string) []string {
-
-	// get all the input letters
-	letters := []string{}
-	for _, r := range digits {
-		letters = append(letters, digitToLetterMap[string(r)])
+	if len(digits) == 0 {
+		return []string{}
 	}
 
-	sol := letterCombinationsBT(letters, len(digits))
-	return sol
+	finalSol = []string{}
+	sol = ""
+
+	// get all the input letters
+	letterGroups := []string{}
+	for _, r := range digits {
+		letterGroups = append(letterGroups, digitToLetterMap[string(r)])
+	}
+
+	// sort letters
+	sort.Strings(letterGroups)
+
+	// remove duplicates
+	letterGroups = slices.Compact(letterGroups)
+
+	// join slice
+	letters := strings.Join(letterGroups, "")
+
+	letterCombinationsBT(letters, len(digits), 0)
+	return finalSol
 }
 
 func main() {
-	r := letterCombinations("23")
+	r := letterCombinations("234")
 	fmt.Println(r)
 }
